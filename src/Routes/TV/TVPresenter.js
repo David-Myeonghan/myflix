4fdc255c1ec3246bd605e172083982355b433949
David MH Ryu <myeonghan12@gmail.com>
2020-03-03 16:48:40 +1000

    edit package.json

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -16,55 +16,62 @@ const TVPresenter = ({ topRated, popular, airingToday, error, loading }) => (
 		<Helmet>
 			<title>TV Shows | Myflix</title>
 		</Helmet>
-		loading ? (
-		<Loader />) : (
-		<Container>
-			{topRated && topRated.length > 0 && (
-				<Section title="Top Rated Shows">
-					{topRated.map(show => (
-						<Poster
-							key={show.id}
-							id={show.id}
-							imageUrl={show.poster_path}
-							title={show.original_name}
-							rating={show.vote_average}
-							year={show.first_air_date && show.first_air_date.substring(0, 4)}
-						/>
-					))}
-				</Section>
-			)}
-			{popular && popular.length > 0 && (
-				<Section title="Popular Shows">
-					{popular.map(show => (
-						<Poster
-							key={show.id}
-							id={show.id}
-							imageUrl={show.poster_path}
-							title={show.original_name}
-							rating={show.vote_average}
-							year={show.first_air_date && show.first_air_date.substring(0, 4)}
-						/>
-					))}
-				</Section>
-			)}
-			{airingToday && airingToday.length > 0 && (
-				<Section title="Airing Today">
-					{airingToday.map(show => (
-						<Poster
-							key={show.id}
-							id={show.id}
-							imageUrl={show.poster_path}
-							title={show.original_name}
-							rating={show.vote_average}
-							year={show.first_air_date && show.first_air_date.substring(0, 4)}
-						/>
-					))}
-					))}
-				</Section>
-			)}
-			{error && <Message text={error} color="#e74c3c" />}
-		</Container>
-		)
+		{loading ? (
+			<Loader />
+		) : (
+			<Container>
+				{topRated && topRated.length > 0 && (
+					<Section title="Top Rated Shows">
+						{topRated.map(show => (
+							<Poster
+								key={show.id}
+								id={show.id}
+								imageUrl={show.poster_path}
+								title={show.original_name}
+								rating={show.vote_average}
+								year={
+									show.first_air_date && show.first_air_date.substring(0, 4)
+								}
+							/>
+						))}
+					</Section>
+				)}
+				{popular && popular.length > 0 && (
+					<Section title="Popular Shows">
+						{popular.map(show => (
+							<Poster
+								key={show.id}
+								id={show.id}
+								imageUrl={show.poster_path}
+								title={show.original_name}
+								rating={show.vote_average}
+								year={
+									show.first_air_date && show.first_air_date.substring(0, 4)
+								}
+							/>
+						))}
+					</Section>
+				)}
+				{airingToday && airingToday.length > 0 && (
+					<Section title="Airing Today">
+						{airingToday.map(show => (
+							<Poster
+								key={show.id}
+								id={show.id}
+								imageUrl={show.poster_path}
+								title={show.original_name}
+								rating={show.vote_average}
+								year={
+									show.first_air_date && show.first_air_date.substring(0, 4)
+								}
+							/>
+						))}
+						))}
+					</Section>
+				)}
+				{error && <Message text={error} color="#e74c3c" />}
+			</Container>
+		)}
 	</>
 );
 TVPresenter.propTypes = {

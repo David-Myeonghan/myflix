07c91834dfcf373b0728de38a96a8f5947e583da
David MH Ryu <myeonghan12@gmail.com>
2020-06-28 20:59:50 +1000

    Updates

---
+++ precache-manifest.27e90942a1de512d939a7c6305f83b1a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c3632f261de874522bc3029f700d75a",
+    "url": "/myflix/index.html"
+  },
+  {
+    "revision": "5fdce5a851a209909971",
+    "url": "/myflix/static/js/2.d25ca7c5.chunk.js"
+  },
+  {
+    "revision": "c12614f2ae04b8c59c86",
+    "url": "/myflix/static/js/main.c32b0971.chunk.js"
+  },
+  {
+    "revision": "db2a3e6ae6278b493619",
+    "url": "/myflix/static/js/runtime-main.a6470e5e.js"
+  }
+]);
